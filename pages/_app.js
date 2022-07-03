7456aa695039d4c2c64d0d0aad15a14219ac8e31
PowerRanger1a <108588059+PowerRanger1a@users.noreply.github.com>
2022-07-03 14:10:38 +0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,7 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <title key={"title"}>Power Rangers Free</title>
-        <link rel="icon" href="/icon1.webp" />
+        <link rel="icon" href="/logo.webp" />
 
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta name="description" content="Online power ranger games" />
